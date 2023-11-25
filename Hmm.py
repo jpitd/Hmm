41c16ab164b72cbe5b37492c9029fe37e1a711fc
jpitd <128343532+jpitd@users.noreply.github.com>
2023-11-25 18:47:02 +0800

    Hmm

--- Hmm.py
+++ Hmm.py
@@ -3,14 +3,16 @@ import numpy as np
 
 class Hmm:
     def __init__(self):
-        self.state_number = 3
-        self.observation_number = 2
+        """用于将HMM初始化"""
+        self.state_number = 3  # 设置状态变量的数量
+        self.observation_number = 2  # 设置观测变量的数量
         self.state_transition_matrix = np.array([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]],
-                                                dtype=np.float64)
-        self.observation_matrix = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]], dtype=np.float64)
-        self.initial_state_possibility_matrix = np.array([[0.2], [0.4], [0.4]], dtype=np.float64)
+                                                dtype=np.float64)  # 设置状态转移矩阵的初值
+        self.observation_matrix = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]], dtype=np.float64)  # 设置观测生成矩阵的初值
+        self.initial_state_possibility_matrix = np.array([[0.2], [0.4], [0.4]], dtype=np.float64)  # 设置初始状态分布矩阵
 
     def learning_option(self):
+        """用于重新设置HMM的初值，该函数只是为了验证学习算法的正确性而写，可直接删除"""
         self.state_number = 3
         self.observation_number = 2
         self.state_transition_matrix = np.array([[0.1, 0.6, 0.4], [0.5, 0.4, 0.1], [0.3, 0.5, 0.2]],
@@ -18,29 +20,34 @@ class Hmm:
         self.observation_matrix = np.array([[0.4, 0.6], [0.1, 0.9], [0.2, 0.8]], dtype=np.float64)
         self.initial_state_possibility_matrix = np.array([[0.6], [0.1], [0.3]], dtype=np.float64)
 
-    def forward(self, observation_que, judge="true"):
-        forward_parameter = self.initial_state_possibility_matrix * self.observation_matrix[:, observation_que[0] - 1]
+    def forward(self, observation_que, judge=True):
+        """前向算法，observation_que为要用到的观测序列，judge的作用是判断是否要进一步求得序列概率"""
+        forward_parameter = (self.initial_state_possibility_matrix.T *
+                             self.observation_matrix[:, observation_que[0] - 1])  # 初始化前向参数
         for i in observation_que[1::]:
-            forward_parameter = np.dot(forward_parameter, self.state_transition_matrix)
-            forward_parameter *= self.observation_matrix[:, i - 1]
+            """该for循环是用于递推前向参数"""
+            forward_parameter = np.dot(forward_parameter, self.state_transition_matrix)  # 通过矩阵内积运算求得状态转移后的概率
+            forward_parameter *= self.observation_matrix[:, i - 1]  # 乘上最后的观测概率得到每个状态的前向概率
         if judge:
             return forward_parameter
         else:
-            return np.sum(forward_parameter)
+            return np.sum(forward_parameter)  # 返回序列概率
 
-    def backward(self, observation_que, judge="true"):
-        backward_parameter = np.ones([self.state_number, 1], dtype=np.float64)
-        for i in observation_que[::-1]:
-            a = self.state_transition_matrix
-            a = a * observation_que[:, i - 1]
-            backward_parameter = np.dot(a, backward_parameter)
+    def backward(self, observation_que, judge=True):
+        """后向算法，observation_que为要用到的观测序列，judge的作用是判断是否要进一步求得序列概率"""
+        backward_parameter = np.ones([self.state_number, 1], dtype=np.float64)  # 初始化后向参数
+        for i in observation_que[:0:-1]:
+            """该for循环是用于递推后向参数"""
+            a = self.state_transition_matrix * self.observation_matrix[:, i - 1:i].T  # 求后向算法公式中的状态转移和观测的部分
+            backward_parameter = np.dot(a, backward_parameter)  # 最后将上面得到的部分和后向概率求内积得到更新的后向概率
         if judge:
             return backward_parameter.T
         else:
             backward_parameter = np.dot(
-                self.initial_state_possibility_matrix * self.observation_matrix[:, observation_que[0]],
-                backward_parameter)
-            return backward_parameter.T
+                self.initial_state_possibility_matrix.T *
+                self.observation_matrix[:, observation_que[0] - 1:observation_que[0]].T,
+                backward_parameter)  # 求序列概率
+            return backward_parameter
 
     def forward_backward(self, observation_que, time, state):
         observation_que1 = observation_que[0:time]
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-import numpy as np
+from Hmm import *
 
-
-
-if __name__=="__main__":
-    a=np.arange(4).reshape(4,1)
-    b=np.array([[1,2,8,4],[5,6,7,8]])
-    c=np.arange(4)
-    print(np.where(b[:,3:4]==8)[0])
+if __name__ == "__main__":
+    a = Hmm()
+    print(a.backward([1, 2, 1],False))
